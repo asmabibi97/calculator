d44b77e85a13d8c92e8fbed8945410c427e5e077
AqibShaheer <65130459+AqibShaheer@users.noreply.github.com>
2024-03-26 12:58:49 -0700

    first project

--- index.ts
+++ index.ts
@@ -20,3 +20,4 @@ else if (answer.operator==="multiplication")
 else if (answer.operator==="division")
 {console.log(answer.firstname / answer.secondname);}
 else {console.log("please wrie a valid operator");}
+console.log('the end');
